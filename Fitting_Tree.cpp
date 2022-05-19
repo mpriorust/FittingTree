f39ce6629ff2050d734bc4f01ca2a9e8cc92637e
Max Prior <mprior@connect.ust.hk>
2022-05-19 14:02:39 +0800

    add: Fitting_Tree.cpp Fitting_Tree.h

---
+++ Fitting_Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max Prior on 19.05.22.
+//
+
+#include "Fitting_Tree.h"
---
+++ Fitting_Tree.h
@@ -0,0 +1,16 @@
+//
+// Created by Max Prior on 19.05.22.
+//
+
+#ifndef FITTINGTREE_FITTING_TREE_H
+#define FITTINGTREE_FITTING_TREE_H
+#include "iostream"
+
+class Fitting_Tree {
+
+};
+
+int main(){
+}
+
+#endif //FITTINGTREE_FITTING_TREE_H
